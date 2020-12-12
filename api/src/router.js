a9b440e1b57588523bea098ee080fec113ae56c4
saharb <saharb@jfrog.com>
2020-12-12 14:07:58 +0200

    module3

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,9 @@
 const {Router} = require('express');
 const router = Router();
 const multer = require('multer');
+const path = require('path');
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
+
 const storage = multer.diskStorage({
     destination: 'api/uploads/',
     filename,
@@ -19,10 +22,18 @@ const fileFilter = (request, file, callback) => {
     }
 };
 
-const upload = multer({fileFilter: fileFilter, storage: storage});
+const upload = multer({
+    fileFilter: fileFilter,
+    storage: storage
+});
+
 router.post('/upload', upload.single('photo'), (request, response) => {
     if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
 
     return response.status(201).json({success: true});
+});
+
+router.get('/photo-viewer', (request, response) => {
+   response.sendFile(photoPath)
 });
 module.exports = router;
