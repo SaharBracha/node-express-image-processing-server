8accfdc956888ff73af5b7311d9d50416d772b2b
saharb <saharb@jfrog.com>
2020-12-12 13:12:42 +0200

    module1

---
+++ api/src/router.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const storage = require('multer').diskStorage({destination: 'api/uploads/', filename: filename});
+const upload = require('multer').multer({fileFilter: fileFilter(), storage: storage});
+
+function filename(request, file, callback) {
+    callback(null, file.originalname)
+}
+
+function fileFilter(request, file, callback) {
+    if (file.mimeType !== 'image/png') {
+        request.fileValidationError = 'Wrong file type';
+        callback(null, false, new Error('Wrong file type'));
+    } else {
+        callback(null, true);
+    }
+}
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+    if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
+
+    return response.status(201).json({success: true});
+});
+module.exports = router;
